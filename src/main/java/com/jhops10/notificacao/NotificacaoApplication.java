097b4b4e16f7a2a0c65cdc0e364910ea312ffbf2
João Paulo <joaowais@gmail.com>
2024-09-19 10:46:40 -0300

    commit inicial

---
+++ src/main/java/com/jhops10/notificacao/NotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoApplication.class, args);
+	}
+
+}
