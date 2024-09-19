aee3c928c9fe3a2e162b603709543d5f738a55e5
João Paulo <joaowais@gmail.com>
2024-09-19 13:14:16 -0300

    adicionados metodos de envio de email

---
+++ src/main/java/com/jhops10/notificacao/business/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,5 @@
+package com.jhops10.notificacao.business.enums;
+
+public enum StatusNotificacaoEnum {
+    PENDENTE, NOTIFICADO, CANCELADO
+}
---
+++ src/main/java/com/jhops10/notificacao/infrastructure/exceptions/EmailException.java
@@ -0,0 +1,12 @@
+package com.jhops10.notificacao.infrastructure.exceptions;
+
+public class EmailException extends RuntimeException {
+
+    public EmailException(String msg) {
+        super(msg);
+    }
+
+    public EmailException(String msg, Throwable throwable) {
+        super(msg, throwable);
+    }
+}
